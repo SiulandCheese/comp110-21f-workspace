f28a0a03b5bb84cdd8adc5d40e2769f0aec2376a
Luis Sanchez <luis.salvador.sanchez@gmail.com>
2021-09-30 00:54:52 -0400

    Finished utils.py

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -4,6 +4,7 @@ __author__ = "730383189"
 
 
 def all(xs: list[int], ys: int) -> bool: 
+    """Helps to find if all items are equal to an integer."""
     i: int = 0
     true_cases: int = 0
     while i < len(xs): 
@@ -13,49 +14,87 @@ def all(xs: list[int], ys: int) -> bool:
         else: 
             i += 1
             break
-    return true_cases == i
+    return true_cases == i and len(xs) > 0
 
 
-xs = [2, 2, 2] 
+xs: list[int] = [] 
 ys = 2
 
 print(all(xs, ys))
 
 
+# def is_equal(a: list[int], b: list[int]) -> bool: 
+#     """Helps to find if all items in two strings are equal."""
+#     i = 0 
+#     j = 0 
+#     k = 0 
+#     while j < len(b): 
+#         while i < len(a): 
+#             if a[i] == b[j]: 
+#                 i += 1
+#                 k += 1
+#             else: 
+#                 break
+#         j += 1
+#         i = 0 
+#     return len(a) * len(b) == k 
+
 def is_equal(a: list[int], b: list[int]) -> bool: 
-    i2 = 0 
-    j = 0 
-    k = 0 
-    while j < len(b): 
-        while i2 < len(a): 
-            if a[i2] == b[j]: 
-                i2 += 1
-                k += 1
+    """Helps to find if all items in two strings are equal."""
+    i = 0 
+    j = 0
+    if len(a) > len(b): 
+        while i < len(b): 
+            if a[i] == b[i]: 
+                j += 1
+                i += 1
             else: 
-                i2 += 1
-        j += 1
-    return i2 == k 
+                break 
+        return len(a) == j
+    elif len(b) > len(a): 
+        while i < len(a): 
+            if a[i] == b[i]: 
+                j += 1
+                i += 1
+            else: 
+                break 
+        return len(b) == j 
+    elif len(a) == len(b): 
+        while i < len(a): 
+            if a[i] == b[i]: 
+                j += 1
+                i += 1
+            else: 
+                break 
+        return len(a) == j 
 
 
-a = [1, 1, 2]
-b = [1, 1]
+a = [1, 2, 3]
+b = [2, 3]
 
 print(is_equal(a, b))
 
 
 def max(c: list[int]) -> int: 
+    """Helps to find largest number in a string."""
     m = 0 
     n = 0 
+    o = 0 
     while m < len(c): 
-        if c[m] > c[0] - 1: 
-            n = c[m]
-            m += 1
-        else: 
-            m += 1
+        while n < len(c): 
+            if c[m] >= c[n]: 
+                n += 1 
+                if n == len(c):
+                    o = c[m] 
+                    break  
+            else: 
+                n = 0 
+                break 
+        m += 1
     if len(c) == 0: 
         raise ValueError("max() arg is an empty List")
-    return n 
+    return o 
 
 
-c = [100, 10000, 102931]
+c = [1, 2, 3, 4, 5]
 print(max(c))
