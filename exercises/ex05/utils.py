29dd7ad14806877da0d0bc7622e134c1b3c3a11b
Luis Sanchez <luis.salvador.sanchez@gmail.com>
2021-10-06 23:00:15 -0400

    ex05 Attempt #1

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,42 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730383189"
 
 # Define your functions below
+
+
+def only_evens(a: list[int]) -> list[int]:
+    i: int = 0 
+    b: list[int] = []
+    while i < len(a): 
+        if a[i] % 2 == 0: 
+            b.append(a[i])
+    i += 1
+
+    return b
+
+
+def sub(a: list[int], b: int, c: int) -> list[int]: 
+    i: int = 0 
+    list_2: list[int] = []
+    while i < len(a): 
+        if i >= b and i < c:
+            list_2.append(a[i])
+        i += 1 
+    
+    return list_2
+
+
+def concat(a: list[int], b: list[int]) -> list[int]: 
+    i: int = 0 
+    c: list[int] = []
+    while i < len(a):
+        c.append(a[i])
+        i += 1
+    
+    i = 0 
+    while i < len(b): 
+        c.append(b[i])
+        i += 1
+    
+    return c
