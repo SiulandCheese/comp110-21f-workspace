67edf38ccf03859d7923088165e201772de1797b
Luis Sanchez <luis.salvador.sanchez@gmail.com>
2021-10-13 22:50:35 -0400

    First Attempt EX06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,62 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730383189"
 
-# Define your functions below
+
+def invert(test: dict[str, str]) -> None:
+    """Makes keys values and values keys."""
+    i = 0 
+    j = 1 
+    new_dict: dict[str, str] = dict()
+    values: list[str] = list(test.values())
+    keys: list[str] = list(test.keys())
+
+    while j < len(values): 
+        if values[i] == values[j]: 
+            raise KeyError("Cannot have two keys of the same type") 
+        if j == len(values) - 1: 
+            i += 1
+            j = i
+        j += 1
+
+    i = 0 
+    while i < len(test):
+        new_dict[values[i]] = keys[i]
+        i = i + 1
+
+    print(new_dict)
+
+
+def favorite_colors(NAMES_COLORS: dict[str, str]) -> str:
+    """Finds which value in a dictionary is most common.""" 
+    colors: list[str] = list(NAMES_COLORS.values())
+    i = 1 
+    j = 0
+    k = 0 
+    m = 0 
+    common_color: str = ""
+    while j < len(colors): 
+        while i < len(colors): 
+            if colors[i] == colors[j]: 
+                k += 1 
+            if k > m: 
+                m = k 
+                common_color = colors[j]
+            i += 1 
+        j += 1
+        i = j + 1 
+        k = 0 
+    return common_color
+
+
+def count(words: list[str]) -> dict[str, int]:
+    """Counts the amount of times words in a list are used."""
+    words_dict: dict[str, int] = dict()
+    i = 0 
+    for word in words: 
+        if words[i] in words_dict: 
+            words_dict[words[i]] += 1
+        else: 
+            words_dict[words[i]] = 1 
+        i += 1
+    return words_dict
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,58 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_colors, count
+import pytest
+__author__ = "730383189"
 
-__author__ = "123456789"
+
+def test_invert_KeyError() -> None:
+    """Tests if invert raises KeyError."""
+    with pytest.raises(KeyError): 
+        my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
+        invert(my_dictionary)
+
+
+def test_invert_my_birthday() -> None: 
+    """Tests if invert works with my birhday."""
+    assert invert({"12": "21"}) is None
+
+
+def test_invert_empty() -> None: 
+    """Tests if invert works while empty."""
+    assert invert({}) is None
+
+
+def test_favorite_colors_study_group() -> None: 
+    """Tests if favorite colors works with my study group."""
+    NAMES_COLORS: dict[str, str] = {"Parker": "Purple", "Vivian": "Periwinkle", "Luis": "Purple"}
+    assert favorite_colors(NAMES_COLORS) == 'Purple'
+
+
+def test_favorite_colors_family() -> None: 
+    """Tests if favorite colors works with my family."""
+    NAMES_COLORS: dict[str, str] = {"Dad": "Green", "Mom": "Red", "Me": "Purple", "Sister": "Red"}
+    assert favorite_colors(NAMES_COLORS) == 'Red'
+
+
+def test_favorite_colors_empty() -> None:
+    """Tests if favorite colors works empty."""
+    NAMES_COLORS: dict[str, str] = {}
+    assert favorite_colors(NAMES_COLORS) == ''
+
+
+def test_count_sun_tzu() -> None:
+    """Tests if count works on sun tzu."""
+    words: list[str] = ["All", "Warfare", "Is", "Based", "On", "Deception"]
+    assert count(words) == {"All": 1, "Warfare": 1, "Is": 1, "Based": 1, "On": 1, "Deception": 1}
+
+
+def test_count_shakesphere() -> None:
+    """Tests if count works on shakesphere."""
+    words: list[str] = ["To", "Be", "Or", "Not", "To", "Be"]
+    assert count(words) == {"To": 2, "Be": 2, "Or": 1, "Not": 1}
+
+
+def test_count_empty() -> None:
+    """Tests if count works empty."""
+    words: list[str] = []
+    assert count(words) == {}
